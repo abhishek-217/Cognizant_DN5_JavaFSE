7d395cac9577d8356b365d9edda1f375d44ecdc2
abhishek-217 <abhiman5196@gmail.com>
2026-07-06 23:22:07 +0530

    First Spring application complete

---
+++ Week_02/Spring Core and Maven/LibraryManageSystem/src/main/java/com/library/LibraryManageApp.java
@@ -0,0 +1,16 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManageApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                LibraryManageApp.class,
+                args);
+
+    }
+}
